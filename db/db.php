ecc5e669f3878baba55fbdd4ac214ca9948458c0
Lokesh <vattamlokesh03@gmail.com>
2026-06-11 22:57:41 +0530

    Project set up completed

---
+++ db/db.php
@@ -0,0 +1,8 @@
+<?php
+$conn =mysqli_connect('localhost','root','','employee_datails');
+if(!$conn){
+	die("failed");
+}else{
+	
+}
+?>
---
+++ includes/include.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>Employee Registration</title>
+	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet">
+	<link rel="stylesheet"  href="assets/style.css">
+  
+</head>
+<body>
+<form>
+	<label>Employee Name</label>
+	<input type="text" name="useename">
+	<br><br>
+	<label>Password</label>
+	
+	<input type="text" name="password">
+	<br><br>
+	<input type="submit" name="submit">
+</form>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js"></script>
+</body>
+</html>
